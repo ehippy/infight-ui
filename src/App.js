8cbb24f55d3ca87e52072eaea8cb7a5fe789f175
Patrick McDavid <pmcdavid@gmail.com>
2017-08-23 16:32:22 -0600

    bootstrap, also login logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: localStorage.getItem('infight_jwt') !== undefined
+        };
 
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        let url = new URL(window.location.href);
+        let c = url.searchParams.get("cookie");
+        console.log(c);
 
-          <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.team,identity.avatar&client_id=134879189280.222167489812"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
+        if (c) {
+            localStorage.setItem('infight_jwt', c);
+            window.location.href = url.origin;
+        }
+        this.logOut = this.logOut.bind(this);
 
-          </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. brah!
-        </p>
-      </div>
-    );
-  }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row App-header">
+                    <div className="col">infight</div>
+                    <div className="col">
+                        {this.identityUI()}
+                    </div>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload. brah!
+                </p>
+            </div>
+
+        );
+    }
+
+    logOut() {
+        localStorage.removeItem('infight_jwt');
+        this.setState({loggedIn: false});
+    }
+
+    identityUI() {
+
+        if (this.state.loggedIn) {
+            return (
+                <div>
+                    <div>U R LOGGED</div>
+                    <a onClick={this.logOut}>Log Out</a>
+                </div>
+            )
+        } else {
+            return (
+                <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.team,identity.avatar&client_id=134879189280.222167489812"
+                   className="float-right">
+                    <img alt="Sign in with Slack" height="40" width="172"
+                         src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+                         srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"/>
+                </a>
+            )
+        }
+
+    }
 }
 
 export default App;
