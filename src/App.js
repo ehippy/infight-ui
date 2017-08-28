3a4f6cfe6e075dfabd4a64bfea38b15cba2be8c9
Patrick McDavid <pmcdavid@gmail.com>
2017-08-27 21:29:37 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import logo from './infight.svg'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
 let jwtDecode = require('jwt-decode');
 
@@ -13,24 +18,43 @@ class App extends Component {
         };
 
         this.state = this.checkLogin(initialState);
+
         this.logOut = this.logOut.bind(this);
+        this.startGame = this.startGame.bind(this);
     }
+
+    siteHome = () => (
+        <div className="col">
+            <h2>Welcome to infight</h2>
+        </div>
+    );
+
+    teamHome = ({match}) => (
+        <div className="row">
+            <div className="col">
+                <h2>{match.params.team_domain}</h2>
+                {this.teamUI()}
+            </div>
+        </div>
+    );
 
     render() {
         return (
-            <div className="container">
-                <div className="row App-header">
-                    <div className="col"><img src={logo} className="HeaderLogo" alt="infight"/> infight</div>
-                    <div className="col">
-                        {this.identityUI()}
+            <Router>
+                <div className="container">
+                    <div className="row App-header">
+                        <div className="col">
+                            <Link to="/"><img src={logo} className="HeaderLogo" alt="infight"/> infight</Link>
+                        </div>
+                        <div className="col">
+                            {this.identityUI()}
+                        </div>
                     </div>
+
+                    <Route exact path="/" component={this.siteHome}/>
+                    <Route path="/:team_domain" component={this.teamHome} />
                 </div>
-                <div className="row">
-                    <div className="col">
-                        {this.teamUI()}
-                    </div>
-                </div>
-            </div>
+            </Router>
 
         );
     }
@@ -60,20 +84,13 @@ class App extends Component {
         if (userDetails !== null){
             let app = this;
             if (!localStorage.getItem('foundTeam')) {
-                fetch(App.getApiBase() + "/" + userDetails.team_domain + "/exists", {
-                    headers: {'Authorization': 'Basic ' + localStorage.getItem('infight_jwt')}
-                })
-                    .then(response => {
-                        console.log(response);
-                        if (response.ok) {
-                            response.json().then(foundTeam => {
-                                app.setState({mustAddToSlack: !foundTeam});
-                                if (foundTeam) {
-                                    localStorage.setItem('foundTeam', true)
-                                }
-                            });
-                        }
-                    });
+                this.authedApiCall("/" + userDetails.team_domain + "/exists", {}, (err, data)=>{
+                    if (err) return;
+                    app.setState({mustAddToSlack: !data});
+                    if (data) {
+                        //localStorage.setItem('foundTeam', true)
+                    }
+                });
             }
         }
 
@@ -87,6 +104,33 @@ class App extends Component {
         this.setState({loggedIn: false});
     }
 
+    startGame() {
+        let app = this;
+        this.authedApiCall("/" + app.state.user.team_domain + "/game", {method:'POST'}, (err, data)=>{
+            if (err) {return alert('it broke')}
+            console.log(data);
+        })
+    }
+
+    authedApiCall(path, fetchOptions = {}, callback) {
+        if (typeof fetchOptions.headers !== "object") {
+            fetchOptions.headers = {}
+        }
+        fetchOptions.headers.Authorization  ='Basic ' + localStorage.getItem('infight_jwt');
+
+        fetch(App.getApiBase() + path, fetchOptions)
+            .then(response => {
+                console.log(response);
+                if (response.ok) {
+                    response.json().then(response => {
+                         callback(null, response);
+                    });
+                } else {
+                    callback("Bad Response", response)
+                }
+            });
+    }
+
     teamUI() {
         if (this.state.mustAddToSlack) {
             return (
@@ -95,9 +139,13 @@ class App extends Component {
 
                     <a href={"https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=134879189280.222167489812&redirect_uri=" + encodeURIComponent(App.getApiBase() + '/install')}>
                         <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
-                             srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+                             srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
                     </a>
                 </div>
+            )
+        } else {
+            return (
+                <a onClick={this.startGame}>Start a game</a>
             )
         }
     }
@@ -110,7 +158,7 @@ class App extends Component {
                     <div>
                         <img className="User-header-img" src={this.state.user.user_img} alt={this.state.user.user_name}/>
                         <span>{this.state.user.user_name}</span>
-                        <span className="HeaderTeamName">{this.state.user.team_domain}</span>
+                        <Link className="HeaderTeamName" to={'/'+this.state.user.team_domain}>{this.state.user.team_domain}</Link>
                         <a onClick={this.logOut} className="AppLogOut">Log Out</a>
                     </div>
                 </div>
